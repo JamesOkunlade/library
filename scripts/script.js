1d7fb7527b9463c0249111b6429569648aa926b3
James Okunlade <james.okunlade@gmail.com>
2019-06-26 19:50:30 +0100

    library frontend with basic styling

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+/// Modal scripts
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+
+/// Library scripts
+
+// let myLibrary = [];
+//
+// function Book() {
+//   // the constructor
+// }
+//
+//
+// function addBookToLibrary() {
+//   // Do this
+// }
